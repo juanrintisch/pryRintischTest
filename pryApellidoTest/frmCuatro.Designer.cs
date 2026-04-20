4e5a7157e2fc87e9a262a2f48ce4e1f23659644d
juanrintisch <juanrintisch@gmail.com>
2026-04-20 09:50:53 -0300

    a la mitad

---
+++ pryApellidoTest/frmCuatro.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace pryApellidoTest
+{
+    partial class frmCuatro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCuatro));
+            this.SuspendLayout();
+            // 
+            // frmCuatro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(531, 409);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmCuatro";
+            this.Text = "frmResumen";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
